96dd6b07ddc03c01172af2c724fa542a35f24123
Carine Bruyndoncx <bruyndoncx@gmail.com>
2024-07-09 23:25:04 +0200

    Refactored imports and function calls in main.py.

--- main.py
+++ main.py
@@ -14,13 +14,10 @@ import pandas as pd
 import argparse
 from logger import ComplexLogger
 
-from generate_crew import read_variables_xls, snake_case
-from importlib import import_module
-
-import init_config
-import excel_operations
-import crew_operations
-import gradio_interface
+from init_config import create_default_dir, init_default_dirs, init_logging, read_logs
+from excel_operations import sanitize_for_excel, open_workbook, save_workbook, write_log_sheet, add_md_files_to_log_sheet, list_xls_files_in_dir, get_distinct_column_values_by_name
+from crew_operations import module_callback, run_crew, get_crew_jobs_list, get_crew_job, setup, get_crews_details, get_jobs_details, get_crews_jobs_from_template, upload_file, extract_variables, map_variables_to_ui_fields, get_ui_field_labels, get_mapped_variables, get_input_mapping, parse_details, get_jobdetails
+from gradio_interface import run_gradio
 
 #############################################################################
 # START PROCESSING
